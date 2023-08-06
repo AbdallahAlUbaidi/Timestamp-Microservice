cbc89c774d7e21648725f321fd0121ef1fda1f37
AbdallahAlUbaidi <Abdallah.Alubaidi111@gmail.com>
2023-08-06 06:57:56 +0300

    Added timestamp API end point

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,9 +19,18 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/:timeStamp", function (req, res) {
+  let input = req.params.timeStamp;
+  if (/^\d+$/.test(input)) date = new Date(Number(input));
+  else {
+    const [year, month, day] = input.split("-").map(Number);
+    let currentHour = new Date().getHours();
+
+    date = new Date(year, month - 1, day + 1 || 2, -currentHour);
+  }
+  if (isNaN(date)) return res.json({ error: "Invalid Date" });
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
 
